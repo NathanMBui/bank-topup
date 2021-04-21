8cde4aded7851efd77714e7a59ca2f4ab6588400
Nathan <buiminhat@gmail.com>
2021-04-21 11:43:21 +0700

    using lombok to reduce boilerplate codes

--- topup-service/src/main/java/com/abcbank/topup/entities/Voucher.java
+++ topup-service/src/main/java/com/abcbank/topup/entities/Voucher.java
@@ -1,10 +1,15 @@
 package com.abcbank.topup.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "Voucher")
+@Getter
+@Setter
 public class Voucher {
 
     @Id
@@ -32,60 +37,4 @@ public class Voucher {
 
     @Column(name = "description")
     private String description;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getProvider() {
-        return provider;
-    }
-
-    public void setProvider(String provider) {
-        this.provider = provider;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- voucher-service/src/main/java/com/abcbank/voucher/VoucherRequest.java
+++ voucher-service/src/main/java/com/abcbank/voucher/VoucherRequest.java
@@ -1,8 +1,13 @@
 package com.abcbank.voucher;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
+@Data
+@AllArgsConstructor
 public class VoucherRequest {
     @NotBlank(message = "phoneNumber is required")
     @Pattern(message = "phoneNumber is invalid",
@@ -14,37 +19,4 @@ public class VoucherRequest {
 
     @NotBlank(message = "type is required")
     private String type;
-
-    public VoucherRequest() {
-    }
-
-    public VoucherRequest(String phone, String provider, String type) {
-        this.phoneNumber = phone;
-        this.provider = provider;
-        this.type = type;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getProvider() {
-        return provider;
-    }
-
-    public void setProvider(String provider) {
-        this.provider = provider;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 }
